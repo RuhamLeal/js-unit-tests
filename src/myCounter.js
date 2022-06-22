7babb69683c90dfee511cac8daf6740223d0edd6
Ruham Leal <ruhamxlpro@hotmail.com>
2022-06-22 00:14:00 -0300

    Adiciona requisito 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -17,8 +17,8 @@
 
 const myCounter = () => {
   var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
-    myArray.push(counter);
+  for (let counterb = 0; counterb <= 3; counterb += 1) {
+    myArray.push(counterb);
     for (var counter = 2; counter <= 3; counter += 1) {
       myArray.push(counter);
     }
