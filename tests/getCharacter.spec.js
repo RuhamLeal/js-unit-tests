aca88514e57f24bc9838da31bc2e2d17e00d9a4b
Ruham Leal <ruhamxlpro@hotmail.com>
2022-06-22 12:54:00 -0300

    Adiciona requisito 9

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -41,9 +41,16 @@ Retorno:
 }
 */
 
+const name = 'ARYA';
+
 describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
   it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
-    fail('Teste vazio!');
+    expect(getCharacter()).toBeUndefined();
+    expect(getCharacter('Arya')).toEqual({ name: 'Arya Stark', class: 'Rogue', phrases: [ 'Not today', 'A girl has no name.' ] });
+    expect(getCharacter('Brienne')).toEqual({ name: 'Brienne Tarth', class: 'Knight', phrases: ['Im No Lady, Your Grace.', 'I, Brienne Of Tarth, Sentence You To Die.'] })
+    expect(getCharacter('Melissandre')).toEqual({ name: 'Melissandre', class: 'Necromancer', phrases: ['Death By Fire Is The Purest Death.', 'For The Night Is Dark And Full Of Terrors.'] })
+    expect(getCharacter((name.toLowerCase()))).toEqual({ name: 'Arya Stark', class: 'Rogue', phrases: [ 'Not today', 'A girl has no name.' ] });
+    expect(getCharacter('Joao pedro')).toBeUndefined();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
     // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
